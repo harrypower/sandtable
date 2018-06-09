955334ec1db8cdb99b8ac3d4527e48b3bba3936c
harrypower <philipkingsmith@gmail.com>
2018-06-09 12:50:05 -0600

    testing

--- getdefines.fs
+++ getdefines.fs
@@ -6,7 +6,7 @@ c-library defines_data
   \c #include <unistd.h>
   \c #include <sys/ioctl.h>
 
-  \c void mydefines() { printf("O_NDELAY %d, O_NOCTTY %d, O_RDWR %d \n",O_NDELAY , O_NOCTTY , O_RDWR ) ; }
+  \c void mydefines(void) { printf("O_NDELAY %d, O_NOCTTY %d, O_RDWR %d \n",O_NDELAY , O_NOCTTY , O_RDWR ) ; }
 
   c­function mydefines mydefines ­­ void -- void
 
