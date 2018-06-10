3e9fa95cc357e9225a525d1d71100331335a76fe
harrypower <philipkingsmith@gmail.com>
2018-06-10 00:53:33 -0600

    config uart

---
+++ config-uart.fs
@@ -0,0 +1,27 @@
+\ config-uart.fs
+
+\    Copyright (C) 2018  Philip King. Smith
+
+\    This program is free software: you can redistribute it and/or modify
+\    it under the terms of the GNU General Public License as published by
+\    the Free Software Foundation, either version 3 of the License, or
+\    (at your option) any later version.
+
+\    This program is distributed in the hope that it will be useful,
+\    but WITHOUT ANY WARRANTY; without even the implied warranty of
+\    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\    GNU General Public License for more details.
+
+\    You should have received a copy of the GNU General Public License
+\    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\
+\    beaglebone black uart pin configuration via cli
+
+
+s\" config-pin p9.24 uart\/n" system
+s\" config-pin p9.26 uart\/n" system
+\ this is /dev/ttyO1
+
+s\" config-pin p9.21 uart\/n" system
+s\" config-pin p9.22 uart\/n" system
+\ this is /dev/ttyO2
