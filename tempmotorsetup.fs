d055a7f895bbea38864f61dcf56729411db35e4f
harrypower <philipkingsmith@gmail.com>
2018-06-15 15:25:57 -0600

    constant fix

--- tempmotorsetup.fs
+++ tempmotorsetup.fs
@@ -3,7 +3,7 @@
 require ./BBB_Gforth_gpio/BBB_GPIO_lib.fs
 
 1 constant bank1
-% 110000000000000000 constant enablexy
+%110000000000000000 constant enablexy
 
 bank1 enablexy BBBiosetup
 BBBiooutput
