081d3aedb7d8e4132679ed5d53a2a2690914eda5
harrypower <philipkingsmith@gmail.com>
2018-06-16 11:37:44 -0600

    testing motor

--- tempmotorsetup.fs
+++ tempmotorsetup.fs
@@ -13,4 +13,30 @@ require ./Gforth-Objects/stringobj.fs
   BBBioset
   BBBiocleanup ;
 
-disable-motors 
+disable-motors
+
+: enablemotors ( -- )
+  bank1 enablexy BBBiosetup
+  BBBiooutput
+  BBBioclear
+  BBBiocleanup ;
+
+0 value uarthandle
+: setupuart1
+  1 serial_open to uarthandle
+  uarthandle B115200 serial_setbaud
+  uarthandle ONESTOPB serial_setstopbits
+  uarthandle PARNONE serial_setparity
+  uarthandle serial_flush ;
+
+: sendtest ( -- uwriten )
+  0xa0 pad c!
+  0x00 pad 1 + c!
+  0x0f pad 2 + c!
+  0xb6 pad 3 + c!
+  0x00 pad 4 + c!
+  uarthandle pad 4 serial_write ;
+
+: readit
+  pad 10 dump ;
+  
