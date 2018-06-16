7a2742b7da8b3125c5a92de2f91e2e4ab0e21081
harrypower <philipkingsmith@gmail.com>
2018-06-16 11:24:44 -0600

    testing motors

--- tempmotorsetup.fs
+++ tempmotorsetup.fs
@@ -1,11 +1,16 @@
 \ tempmotorsetup.fs
 
 require ./BBB_Gforth_gpio/BBB_GPIO_lib.fs
+require serial.fs
+require ./Gforth-Objects/stringobj.fs
 
 1 constant bank1
 %110000000000000000 constant enablexy
 
-bank1 enablexy BBBiosetup
-BBBiooutput
-BBBioset
-BBBiocleanup
+: disable-motors ( -- )
+  bank1 enablexy BBBiosetup
+  BBBiooutput
+  BBBioset
+  BBBiocleanup ;
+
+disable-motors 
