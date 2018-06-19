362fb36943726767d25b5b6eebe5e1780a198fa5
harrypower <philipkingsmith@gmail.com>
2018-06-18 19:44:30 -0600

    readreg testing

--- tmc2208.fs
+++ tmc2208.fs
@@ -137,15 +137,15 @@ object class
     0 buffer 1 + c!
     %1111111 and buffer 2 + c! \ place ureg in buffer and set transfer to read register
     buffer 3 this [current] crc8-ATM buffer 3 + c! \ calculate crc and store in buffer
-    uarthandle buffer 4 serial_write \ 4 = if
-\      uarthandle buffer 12 serial_read 12 = if
+    uarthandle buffer 4 serial_write 4 = if
+      uarthandle buffer 12 serial_read \ 12 = if
 \        buffer 4 + 8 0
 \      else
 \        0 0 1 \ did not get all the data from read
 \      then
 \    else
 \      0 0 2 \ write failed
-\    then
+   then
   ;m method readreg
 end-class tmc2208
 
