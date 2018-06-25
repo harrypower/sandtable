541280f4af5cda223b2096e43cfa9ed96fd32d7a
harrypower <philipkingsmith@gmail.com>
2018-06-24 21:43:29 -0600

    include

--- getdefines.fs
+++ getdefines.fs
@@ -5,7 +5,7 @@ c-library defines_data
   \c #include <fcntl.h>
   \c #include <unistd.h>
   \c #include <sys/ioctl.h>
-  \c #include <spi/spidev.h>
+  \c #include <linux/spi/spidev.h>
 
   \c void mydefines(void) { printf("O_NDELAY %d, O_NOCTTY %d, O_RDWR %d , SPI_IOC_WR_MAX_SPEED_HZ %d \n",O_NDELAY , O_NOCTTY , O_RDWR ,SPI_IOC_WR_MAX_SPEED_HZ ) ; }
 
