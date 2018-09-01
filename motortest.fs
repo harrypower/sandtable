ad081728804c6d351d81db73c738ac3d6cad8b30
harrypower <philipkingsmith@gmail.com>
2018-09-01 00:06:40 -0600

    tpwmthrs again

--- motortest.fs
+++ motortest.fs
@@ -81,8 +81,7 @@ mymotorY disable-motor
 ( IHOLD 0, IRUN 16, IHOLDELAY 1)
 0x11 %00000100                          mymotorx putreg . .
 0x00 %100                               mymotorx putreg . .
-0x12 %10                                mymotorx putreg . .
-0x13 %1                                 mymotorx putreg . .
+0x13 %10                                mymotorx putreg . .
 0x70 %0101010000100010000000            mymotorx putreg . .
 ( PWM_AMPL )
 mymotorX disable-motor
