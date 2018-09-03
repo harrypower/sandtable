d1cdbfe8b5ac96e57adb7fdf3d76c134d172e007
harrypower <philipkingsmith@gmail.com>
2018-09-03 12:59:28 -0600

    pwm_autoscale %1

--- motortest.fs
+++ motortest.fs
@@ -85,8 +85,8 @@ mymotorY disable-motor
 ( GCONF with en_pwm-mode 1 )
 0x13 100                                mymotorx putreg . .
 ( TPWMTHRS )
-0x70 %0110100000000100010000            mymotorx putreg . .
-( freewheel %01 ,pwm_symmetric %1 ,pwm_autoscale %0 ,PWM freq %10 ,PWM_GRAD %1 ,PWM_AMPL %00010000 )
+0x70 %0111100000000100010000            mymotorx putreg . .
+( freewheel %01 ,pwm_symmetric %1 ,pwm_autoscale %1 ,PWM freq %10 ,PWM_GRAD %1 ,PWM_AMPL %00010000 )
 0x6d %1000000000000000100100000         mymotorX putreg . .
 ( sfilt 0,sgt %0 , seimin 0, sedn 00, semax 10, seup 01, semin 0)
 0x14 150                               mymotorX putreg . .
