051f3fba6f6bef4bf41206f092792a30bc66cf21
harrypower <philipkingsmith@gmail.com>
2018-10-19 13:46:14 -0600

    777 change

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ sudo cp index.html /var/www/html/index.html
 sudo cp cgi-get-test.cgi /usr/lib/cgi-bin/cgi-get-test.cgi
 sudo touch /run/cgitest.tmp
 
-sudo chmod 755 /usr/lib/cgi-bin/cgi-get-test.cgi
+sudo chmod 777 /usr/lib/cgi-bin/cgi-get-test.cgi
 sudo chmod 666 /run/cgitest.tmp
