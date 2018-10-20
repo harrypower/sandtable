ce01c779e0c90c8c26060ca6281c28f35a1447f7
harrypower <philipkingsmith@gmail.com>
2018-10-20 11:58:02 -0600

    changed to name

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 sudo rm /var/www/html/index.html
 sudo rm /usr/lib/cgi-bin/cgi-get-test.cgi
 sudo rm /run/cgitest.tmp
-sudo rm /usr/lib/cgi-bin/hometest.cgi
+sudo rm /usr/lib/cgi-bin/gohome.cgi
 
 sudo cp index.html /var/www/html/index.html
 sudo cp cgi-get-test.cgi /usr/lib/cgi-bin/cgi-get-test.cgi
-sudo cp hometest.cgi /usr/lib/cgi-bin/hometest.cgi
+sudo cp gohome.cgi /usr/lib/cgi-bin/gohome.cgi
 sudo touch /run/cgitest.tmp
 
 sudo chmod 755 /usr/lib/cgi-bin/cgi-get-test.cgi
-sudo chmod 755 /usr/lib/cgi-bin/hometest.cgi 
+sudo chmod 755 /usr/lib/cgi-bin/gohome.cgi
 sudo chmod 666 /run/cgitest.tmp
