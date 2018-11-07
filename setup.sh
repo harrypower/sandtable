973dbe0f1a72402b21475f7e6aa7e5bf0d149f44
harrypower <philipkingsmith@gmail.com>
2018-11-06 23:16:30 -0700

    get-pid maybe

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ sudo cp gohome.cgi /usr/lib/cgi-bin/gohome.cgi
 sudo cp directtesting.cgi /usr/lib/cgi-bin/directtesting.cgi
 sudo touch /run/cgitest.tmp
 
-sudo cp BBB_Gforth_gpio/syscalls386.fs
+sudo cp BBB_Gforth_gpio/syscalls386.fs /usr/lib/cgi-bin/syscalls386.fs
 
 sudo chmod 755 /usr/lib/cgi-bin/cgi-get-test.cgi
 sudo chmod 755 /usr/lib/cgi-bin/gohome.cgi
