6ef2d8a6e41ee6a715bc87fc1acda6c38b0cf966
harrypower <philipkingsmith@gmail.com>
2018-11-07 11:10:44 -0700

    removed from setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ sudo rm /var/www/html/index.html
 sudo rm /var/www/html/testing.html
 sudo rm /var/www/html/sandtablestyle.css
 sudo rm /usr/lib/cgi-bin/cgi-get-test.cgi
-sudo rm /usr/lib/cgi-bin/gohome.cgi
 sudo rm /run/cgitest.tmp
 sudo rm /usr/lib/cgi-bin/directtesting.cgi
 
@@ -12,7 +11,6 @@ sudo cp index.html /var/www/html/index.html
 sudo cp testing.html /var/www/html/testing.html
 sudo cp sandtablestyle.css /var/www/html/sandtablestyle.css
 sudo cp cgi-get-test.cgi /usr/lib/cgi-bin/cgi-get-test.cgi
-sudo cp gohome.cgi /usr/lib/cgi-bin/gohome.cgi
 sudo cp directtesting.cgi /usr/lib/cgi-bin/directtesting.cgi
 sudo touch /run/cgitest.tmp
 
