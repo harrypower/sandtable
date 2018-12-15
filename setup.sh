16e4122b0b55a930b0d335a8a1e8aa7f1a2a63ae
harrypower <philipkingsmith@gmail.com>
2018-12-14 18:00:59 -0700

    removed cgi-get-test.cgi

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 sudo rm /var/www/html/index.html
 sudo rm /var/www/html/testing.html
 sudo rm /var/www/html/sandtablestyle.css
-sudo rm /usr/lib/cgi-bin/cgi-get-test.cgi
 sudo rm /usr/lib/cgi-bin/directtesting.cgi
 
 sudo cp index.html /var/www/html/index.html
