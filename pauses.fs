a1468a802422b94c9585a8108741f3c27cae0fae
harrypower <philipkingsmith@gmail.com>
2019-01-03 12:21:21 -0700

    mmm

--- pauses.fs
+++ pauses.fs
@@ -2,8 +2,6 @@ c-library pauses
   \c #include <time.h>
 
   \c struct timespec  tim, tim2;
-  \c tim.tv_sec = 0;
-  \c tim2.tv_nsec = 0;
   \c int donanosleep(&tim, &tim2) {
   \c return( nanosleep(&tim, &tim2) );
   \c }
