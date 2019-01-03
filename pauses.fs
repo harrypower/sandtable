995c94460476dbe82f2203ed52fe7f70b0b23186
harrypower <philipkingsmith@gmail.com>
2019-01-03 12:42:30 -0700

    change to usleep

--- pauses.fs
+++ pauses.fs
@@ -1,13 +1,10 @@
 c-library pauses
-  \c #include <time.h>
+  \c #include <unistd.h>
 
-  \c int donanosleep(int sec, int nano) {
-  \c struct timespec tim, tim2
-  \c tim.tv_sec = (time_t)sec ;
-  \c tim.tv_nsec = (long)nano ;
-  \c return( nanosleep(&tim, &tim2) );
+  \c int dousleep(int usecond ) {
+  \c return( usleep(usecond) );
   \c }
 
-  c-function nanosleep donanosleep n n -- n
+  c-function usleep dousleep n -- n
 
 end-c-library
