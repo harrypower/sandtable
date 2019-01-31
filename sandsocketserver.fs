2b5221bc0a315a6f101d71141806c6a3de81bead
harrypower <philipkingsmith@gmail.com>
2019-01-31 13:18:49 -0700

    more understanding needed

--- sandsocketserver.fs
+++ sandsocketserver.fs
@@ -88,14 +88,13 @@ variable buffer$
   begin
     userver accept-socket to usockfd
     usockfd message-buffer @ mb-maxsize read-socket
-    2dup s\" \r\n" compare 0 <>
-    if
+\    2dup s\" \r\n" compare 0 <>
+\    if
       http-response usockfd write-socket
-      \ dup s>d udto$ buffer$ $! s\"  data recieved\n\n" buffer$ $+! buffer$ $@ usockfd write-socket
       2dup addtolog
       type cr ." ^ message ^" cr
       usockfd close-socket
-    then
+\    then
   again
   userver close-server ;
 
