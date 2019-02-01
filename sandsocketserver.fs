9380e76eab57477a1e5de141bd7f308ced008253
harrypower <philipkingsmith@gmail.com>
2019-01-31 17:31:23 -0700

    increase time

--- sandsocketserver.fs
+++ sandsocketserver.fs
@@ -33,7 +33,7 @@
 require unix/socket.fs
 require sandmotorapi.fs
 
-2000 value stream-timeout
+20000 value stream-timeout
 5354 value sandtable-port#
 1024 value mb-maxsize
 variable message-buffer
