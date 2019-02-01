de89c328231bd1f201bb5843da46c588fcfdd801
harrypower <philipkingsmith@gmail.com>
2019-01-31 17:40:04 -0700

    seems to work now???

--- sandsocketserver.fs
+++ sandsocketserver.fs
@@ -68,13 +68,13 @@ variable buffer$
 
 : http-response ( -- caddr u )
   s\" HTTP/1.1 200 OK\r\n" buffer$ $!
-  s\" Date: Mon, 28 Jan 2019 11:31:00 GMT\r\n" buffer$ $+!
+  \ s\" Date: Mon, 28 Jan 2019 11:31:00 GMT\r\n" buffer$ $+!
   s\" Connection: close\r\n" buffer$ $+!
   s\" Server: Gforth0.79\r\n" buffer$ $+!
   s\" Accept-Ranges: bytes\r\n" buffer$ $+!
   s\" Content-type: text/html; charset=utf-8\r\n" buffer$ $+!
-  s\" Content-Length: 32\r\n" buffer$ $+!
-  s\" Last-Modified: Mon, 28 Jan 2019 10:14:49 GMT\r\n" buffer$ $+!
+  s\" Content-Length: 33\r\n" buffer$ $+!
+  \ s\" Last-Modified: Mon, 28 Jan 2019 10:14:49 GMT\r\n" buffer$ $+!
   s\" \r\n" buffer$ $+!
   s\" <html> message recieved </html>\r\n" buffer$ $+!
   s\" \r\n\r\n" buffer$ $+!
