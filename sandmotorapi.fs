dd3142e49a620101d0ac10b9503798d8ffa2a3b0
harrypower <philipkingsmith@gmail.com>
2019-02-27 00:53:40 -0700

    made better zigzag pattern

--- sandmotorapi.fs
+++ sandmotorapi.fs
@@ -482,7 +482,7 @@ true value yposition  \ is the real location of y motor .. note if value is true
           i nxyamount + ym-min movetoxy false = if 102 throw then
         nxyamount +loop
       \  xm-min ym-min movetoxy false = if 103 throw then
-        border
+        border drop
         false
       endof
       ym of
@@ -492,10 +492,37 @@ true value yposition  \ is the real location of y motor .. note if value is true
           i nxyamount 2 / + xm-max swap movetoxy false = if 101 throw then
           i nxyamount + xm-min swap movetoxy false = if 102 throw then
         nxyamount +loop
-        border
+        border drop
         false
       endof
     endcase
+  restore
+  endtry ;
+
+: zigzag-clean2 ( usteps uxy -- nflag )
+  0 { usteps uxy uxyamount }
+  try
+    uxy case
+      xm of
+        xm-max xm-min - usteps / to uxyamount
+        xm-min ym-min movetoxy false = if 100 throw then
+        usteps 0 do
+          i uxyamount * ym-max movetoxy false = if 101 throw then
+          i uxyamount * uxyamount + ym-min movetoxy false = if 102 throw then
+        loop
+        border true = if 103 throw then
+      endof
+      ym of
+        ym-max ym-min - usteps / to uxyamount
+        xm-min ym-min movetoxy false = if 100 throw then
+        usteps 0 do
+          i uxyamount * xm-max swap movetoxy false = if 101 throw then
+          i uxyamount * uxyamount + xm-min swap movetoxy false = if 102 throw then 
+        loop
+        border true = if 103 throw then
+      endof
+    endcase
+    false
   restore
   endtry ;
 
