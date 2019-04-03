66c48b9186dd6fec5dbb9e1b9032551f0ece9084
Philip Smith <philipkingsmith@gmail.com>
2019-04-03 16:00:12 -0600

    adjusted when two boarders are used in drawline

--- sandmotorapi.fs
+++ sandmotorapi.fs
@@ -350,8 +350,9 @@ true value yposition  \ is the real location of y motor .. note if value is true
     nx1 xm-min < nx2 xm-min < and
     ny1 ym-min < ny2 ym-min < and or
     nx1 xm-max > nx2 xm-max > and
-    ny1 ym-max > ny2 ym-max > and or or if nx2 ny2 boardermove exit then \ line intersects with sandtable but is not on the sandtable 
+    ny1 ym-max > ny2 ym-max > and or or if nx2 ny2 boardermove exit then \ line intersects with sandtable but is not on the sandtable
     pointtest 0 = if \ then both boarders found are simply used
+      nx1 ny1 boardermove drop \ this is prefered rather then diagnal movement to first boarder 
       nbx1 to nsx1
       nby1 to nsy1
       nbx2 to nsx2
