9fceddea4a655af592cec7db449b4e7fdeb4030b
Philip Smith <philipkingsmith@gmail.com>
2019-04-04 12:56:54 -0600

    random squares

--- squaretest.fs
+++ squaretest.fs
@@ -37,9 +37,22 @@
 : nrotsquare { usize ux uy uangle usteps } \ will make an angle square but then rotate it usteps around 360 degrees for a full circle
   usteps 0 ?do usize ux uy uangle 360 usteps / i * + nanglesquare loop ;
 
-: nsquare2 ( usize ux uy -- ) \ draw usize square using drawline starting at ux uy location 
+: nsquare2 ( usize ux uy -- ) \ draw usize square using drawline starting at ux uy location
   { usize ux uy }
   ux uy ux usize + uy drawline .
   ux usize + uy ux usize + uy usize + drawline .
   ux usize + uy usize + ux uy usize + drawline .
   ux uy usize + ux uy drawline . cr ;
+
+require random.fs
+: rndsqures ( namount -- )
+  seed-init
+  0 do?
+    xm-max random \ usize
+    xm-max random
+    xm-max random - \ ux
+    ym-max random
+    ym-max random - \ uy
+    .s
+    nsquare2 
+  loop ;
