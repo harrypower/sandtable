5eb4b5f317cdd3e13ddf2558aae15df4514d6f0e
Philip Smith <philipkingsmith@gmail.com>
2019-04-30 13:02:54 -0600

    cleaned up square test to use drawline only

--- squaretest.fs
+++ squaretest.fs
@@ -1,64 +1,32 @@
 \ squaretest.fs
 
-: nsquare { usize ux uy -- }
-  ux uy movetoxy .
-  usize ux + uy movetoxy .
-  usize ux + usize uy + movetoxy .
-  ux usize uy + movetoxy .
-  ux uy movetoxy . ;
-
-: nanglesquare ( usize ux uy uangle -- ) \ uangle is degrees
-  0 0 { usize ux uy uangle ua ub }
-  ux uy movetoxy .
-
-  uangle s>f pi 180e f/ f*
-  fsin usize s>f f*
-  90e pi 180e f/ f*
-  fsin f/ f>s to ua
-  90 uangle - s>f pi 180e f/ f*
-  fsin ua s>f f*
-  uangle s>f pi 180e f/ f*
-  fsin f/ f>s to ub
-
-  ux ub +
-  uy ua -
-  movetoxy .
-
-  ux ub + ua -
-  uy ua - ub -
-  movetoxy .
-
-  ux ua -
-  uy ub -
-  movetoxy .
-
-  ux uy  movetoxy . ;
-
 : n>square ( usize ux uy uangle -- ) \ uangle is degrees
   0 0 0 0 { usize ux uy uangle ua ub ux1 uy1 }
-
-  uangle s>f pi 180e f/ f*
-  fsin usize s>f f*
-  90e pi 180e f/ f*
-  fsin f/ f>s to ua
-  90 uangle - s>f pi 180e f/ f*
-  fsin ua s>f f*
-  uangle s>f pi 180e f/ f*
-  fsin f/ f>s to ub
+  uangle 360 mod to uangle
+  uangle 0 <> if
+    uangle s>f pi 180e f/ f*
+    fsin usize s>f f*
+    90e pi 180e f/ f*
+    fsin f/ f>s to ua
+    90 uangle - s>f pi 180e f/ f*
+    fsin ua s>f f*
+    uangle s>f pi 180e f/ f*
+    fsin f/ f>s to ub
+  else
+    usize to ub
+    usize to ua
+  then
 
   ux uy ux ub + dup to ux1 uy ua - dup to uy1 ( .s ." first " ) drawline . \ testdata
   ux1 uy1 ux ub + ua - dup to ux1 uy ua - ub - dup to uy1 ( .s ." second " ) drawline . \ testdata
   ux1 uy1 ux ua - dup to ux1 uy ub - dup to uy1 ( .s ." third " ) drawline . \ testdata
-  ux1 uy1 ux uy ( .s ." last " ) drawline . \ testdata 
+  ux1 uy1 ux uy ( .s ." last " ) drawline . \ testdata
   ;
 
-: nrotsquare { usize ux uy uangle usteps -- } \ will make an angle square but then rotate it usteps around 360 degrees for a full circle
-  usteps 0 ?do usize ux uy uangle 360 usteps / i * + nanglesquare loop ;
-
-: nrotsquare2 { usize ux uy uangle usteps -- }
+: nrotsquare { usize ux uy uangle usteps -- }
   usteps 0 ?do usize ux uy uangle 360 usteps / i * + n>square loop ;
 
-: nsquare2 ( usize ux uy -- ) \ draw usize square using drawline starting at ux uy location
+: nsquare ( usize ux uy -- ) \ draw usize square using drawline starting at ux uy location
   { usize ux uy }
   ux uy ux usize + uy drawline .
   ux usize + uy ux usize + uy usize + drawline .
@@ -75,7 +43,7 @@ require random.fs
     ym-max 2 / random \ uy
     dup movetoy .
     .s cr
-    nsquare2
+    nsquare
   loop ;
 
 : rndsquares2 ( namount -- )
@@ -85,5 +53,5 @@ require random.fs
     xm-max random - \ usize
     xposition
     yposition
-    nsquare2
+    nsquare
   loop ;
