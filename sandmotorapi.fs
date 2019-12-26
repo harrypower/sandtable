66ce2cc4ef58b72054189880e1d124a56532a838
harrypower <philipkingsmith@gmail.com>
2019-12-26 14:56:54 -0700

    removed betterline.fs and added stuff to sandmotorapi

--- sandmotorapi.fs
+++ sandmotorapi.fs
@@ -671,23 +671,50 @@ true value yposition  \ is the real location of y motor .. note if value is true
   nx2 nxoffset +
   ny2 nyoffset + ;
 
-: deg>rads ( uangle -- f: rrad ) \ unangle from stack gets converted to rads and place in floating stack
-  s>f pi 180e f/ f* ;
+gforthtest true = [if]
+  : deg>rads ( uangle -- f: rrad ) \ unangle from stack gets converted to rads and place in floating stack
+    s>f pi 180e f/ f* ;
+[else]
+  : deg>rads ( uangle -- f: rrad ) \ unangle from stack gets converted to rads and place in floating stack
+    s>f fpi 180e f/ f* ;
+[then]
 
-: (calc-c) ( uangle uqnt -- utotal ustep ) \ used by lines to be the distance offset calulation
-  { uangle uqnt }
-  uangle 90 mod dup
-  deg>rads fsin deg>rads fcos f+ xm-max s>f f*
-  fdup uqnt s>f f/
-  f>s f>s swap ;
-: (calc-x) ( uc uangle -- nx ) \ used by lines to calculate x offset from uangle and c distance from calc-c
-  deg>rads fsin s>f f* f>s ;
-: (calc-y) ( uc uangle -- ny ) \ used by lines to calculate y offset from uangle and c distance from calc-c
-  90 swap - deg>rads fsin s>f f* f>s ;
+: corrodinates? ( nx1 ny1 ndistance uangle -- nx2 ny2 ) \ given nx1 ny1 ndistance and uangle return the nx2 and ny2 corrodiantes
+  0 0 { nx1 ny1 ndistance uangle na nb }
+  ndistance 360 uangle - deg>rads s>f fsin f* f>s to na
+  ndistance 360 uangle - deg>rads s>f fcos f* f>s to nb
+  nx1 na + ny1 nb + ;
 
+: (calc-na) ( uc uangle -- nx ) \ find na
+  { uc uangle } uangle 90 >= if
+    180 90 90 180 uangle - - + - deg>rads fsin
+  else
+    uangle deg>rads fsin
+  then
+  uc s>f f* f>s ;
+
+: (calc-nb) ( uc uangle -- ny ) \ find nb
+  { uc uangle } uangle 90 >= if
+    90 180 uangle - - deg>rads fsin
+  else
+    90 uangle - deg>rads fsin
+  then
+  uc s>f f* f>s ;
+
+0e fvalue fslope
+0e fvalue fXn
+0e fvalue ftablemax
+0e fvalue fdpl ( dots per line to calculate distance to next line )
+0e fvalue fltomin ( lines to min this is how many lines to draw from base line to the edge of the sandtable in the minimum direction )
+0e fvalue fYintercept
 : lines ( nx ny uangle uqnt -- ) \ draw uqnt lines with one intersecting with nx ny with uangle from horizontal
   0 0 1500000 { nx ny uangle uqnt nb na usize }
-  uangle 360 mod to uangle
+  \ uqnt 1 + to uqnt
+  uangle 360 mod 180 >= if
+    uangle 360 mod 180 - to uangle
+  else
+    uangle 360 mod to uangle
+  then
   uangle 0 <> if
     uangle deg>rads   \ remember fsin uses rads not angles so convert
     fsin usize s>f f*
@@ -706,26 +733,86 @@ true value yposition  \ is the real location of y motor .. note if value is true
   nx nb + ny na - \ + direction from nx ny
   to nbasey2 to nbasex2
   \ this is the line that intersects with nx ny point
-  uangle uqnt (calc-c) to usize drop
-  nbasex1 nbasey1 nbasex2 nbasey2
-  uangle uqnt (calc-c) drop uangle (calc-x) 0 swap -
-  uangle uqnt (calc-c) drop uangle (calc-y) 0 swap -
-  offset-line \ calculate start line with offset from base line
-  to nyj2 to nxj2 to nyj1 to nxj1
-  uqnt 2 * 0 ?do
-    i usize * uangle (calc-x) to na
-    i usize * uangle (calc-y) to nb
-    nxj1 nyj1 nxj2 nyj2 na nb offset-line
-    .s drawline . cr
-    nxj2 nyj2 nxj1 nyj1 na nb offset-line
-    usize uangle (calc-x)
-    usize uangle (calc-y)
-    offset-line \ add offset for second line
-    .s drawline . cr
-  2 +loop
-  \ border ." boarder " . cr
-  nbasex1 nbasey1 nbasex2 nbasey2 order-line
-  .s drawline . cr
+  uangle 90 =  uangle 0 = or if \ this is for angles 0,90,180 or 270 only
+    uangle 0 = if
+      \ need to solve fdpl and nxj1,nxj2,nyj1,nyj2 amounts for 0 or 180 degrees horizontal line
+      ym-max s>f uqnt s>f f/ to fdpl
+      ny s>f fdpl f/ to fltomin
+      fltomin f>s fdpl f>s * to nb
+      0 to na
+      nbasex1 nbasey1 nbasex2 nbasey2 na 0 swap - nb 0 swap - offset-line order-line
+      to nyj2 to nxj2 to nyj1 to nxj1
+      uqnt 0 ?do
+          0 to na
+          i fdpl f>s * to nb
+          nxj1 nyj1 nxj2 nyj2 na nb offset-line order-line .s drawline . cr
+      loop
+    then
+    uangle 90 = if
+      \ need to solve fdpl and nxj1,nxj2,nyj1,nyj2 amounts for 90 or 270 degrees vertical line
+      xm-max s>f uqnt s>f f/ to fdpl
+      nx s>f fdpl f/ to fltomin
+      0 to nb
+      fltomin f>s fdpl f>s * to na
+      nbasex1 nbasey1 nbasex2 nbasey2 na 0 swap - nb 0 swap - offset-line order-line
+      to nyj2 to nxj2 to nyj1 to nxj1
+      uqnt 0 ?do
+          i fdpl f>s * to na
+          0 to nb
+          nxj1 nyj1 nxj2 nyj2 na nb offset-line order-line .s drawline . cr
+      loop
+    then
+  else
+    \ calculate slope from this base line
+    nbasey1 nbasey2 - s>f
+    nbasex1 nbasex2 - s>f
+    f/ \ slope in floating stack ( f: fslope )
+    fdup to fslope
+    \ use B = Y - ( m * X ) to solve for this y intercept
+    nx s>f f*
+    ny s>f fswap f- fdup to fYintercept \ y intercept in floating stack  ( f: fYintercept )
+    uangle 90 < if
+      90 uangle - deg>rads fsin f* to fXn
+      \ solve y intercept for tablemax
+      fslope xm-max s>f f*
+      ym-max s>f fswap f-  \ ( f: fYintereceptmax )
+      90 uangle - deg>rads fsin f*  fdup to ftablemax ( f: ftablemax )
+      uqnt s>f f/ fdup to fdpl  ( f: fdpl )
+      fXn fswap f/ fdup to fltomin    ( f: fltomin )
+      f>s fdpl f>s * dup
+      uangle (calc-na) to na
+      uangle (calc-nb) to nb
+      nbasex1 nbasey1 nbasex2 nbasey2 na 0 swap - nb 0 swap - offset-line order-line
+      to nyj2 to nxj2 to nyj1 to nxj1
+      uqnt 0 ?do
+          i fdpl f>s * dup
+          uangle (calc-na) to na
+          uangle (calc-nb) to nb
+          nxj1 nyj1 nxj2 nyj2 na nb offset-line order-line .s drawline . cr
+      loop
+    else \ this is for uangle > 90 < 180
+      \ fslope known, fYintercept know
+      \ solve for xn
+      ym-max s>f fYintercept f-
+      90 180 uangle - - deg>rads fsin f* to fXn
+      nx ny xm-max ym-min distance? s>f fxn f+ fdup to ftablemax
+      uqnt s>f f/ fdup to fdpl
+      fXn fswap f/ fdup to fltomin
+      f>s fdpl f>s * dup
+      uangle (calc-na) to na
+      uangle (calc-nb) to nb
+      nbasex1 nbasey1 nbasex2 nbasey2 na 0 swap - nb  offset-line order-line
+      to nyj2 to nxj2 to nyj1 to nxj1
+      uqnt 0 ?do
+          i fdpl f>s * dup
+          uangle (calc-na) to na
+          uangle (calc-nb) to nb
+          nxj1 nyj1 nxj2 nyj2 na nb 0 swap - offset-line order-line .s drawline . cr
+      loop
+    then
+  then
+  \ redraw base line and place ball at nx ny location
+  nbasex1 nbasey1 nbasex2 nbasey2 order-line .s drawline . cr
   nx ny movetoxy . cr  ;
 
 : zigzag-line ( nsteps uxy -- nflag ) \ nflag is false if all ok other numbers are errors
