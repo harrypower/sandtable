51c93f2d1d914f43e4b53bebd8d440f0452ab4a7
harrypower <philipkingsmith@gmail.com>
2020-02-08 12:59:26 -0700

    added favicon.ico

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ sudo rm /var/www/html/index.html
 sudo rm /var/www/html/testing.html
 sudo rm /var/www/html/sandtablestyle.css
 sudo rm /usr/lib/cgi-bin/directtesting.cgi
+sudo rm /var/www/html/favicon.ico
 
 sudo cp index.html /var/www/html/index.html
 sudo cp testing.html /var/www/html/testing.html
 sudo cp sandtablestyle.css /var/www/html/sandtablestyle.css
 sudo cp directtesting.cgi /usr/lib/cgi-bin/directtesting.cgi
+sudo cp favicon.ico /var/www/html/favicon.ico
 
 sudo chmod 755 /usr/lib/cgi-bin/directtesting.cgi
