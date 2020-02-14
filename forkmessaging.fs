b8fa1c3a72a462e84c79adf62127c176a22914cf
harrypower <philipkingsmith@gmail.com>
2020-02-14 14:32:32 -0700

    added epoll stuff again

--- forkmessaging.fs
+++ forkmessaging.fs
@@ -28,6 +28,7 @@ c-library forkmessaging
   \c #include <sys/wait.h>
   \c #include <sys/types.h>
   \ need to export these types used in functions below to use the functions but this will be later as i need them
+  \c #include <sys/epoll.h>
 
 
   c-function fork     fork    void -- n   ( -- npid ) \ fork process npid is 0 to child forked process and a non zero non repeated pid for the parent process
@@ -39,4 +40,32 @@ c-library forkmessaging
   c-function wait     wait    a -- n      ( a*wstatus -- npid_t )
   c-function pipe     pipe    a -- n      ( apipefd[2] -- n )
   c-function pipe2    pipe2   a n -- n    ( apipefd[2] nflags -- n )
+
+  c-function epoll_create   epoll_create  n -- n        ( nsize -- nfd )
+  c-function epoll_create1  epoll_create1 n -- n        ( nflag -- nfd )
+  c-function epoll_ctl      epoll_ctl     n n n a -- n  ( nepfd nop nfd astructevent -- nflag )
+  c-function epoll_wait     epoll_wait    n a n n -- n  ( nfd astructevent nmaxevents ntimeout -- neventcount )
+
 end-c-library
+
+1 constant EPOLL_CTL_ADD \ Add a file decriptor to the interface.
+2 constant EPOLL_CTL_DEL \ Remove a file decriptor from the interface.
+3 constant EPOLL_CTL_MOD \ Change file decriptor epoll_event structure.
+
+\ EPOLL_EVENTS used in epoll_event struct below for events field
+0x001 constant EPOLLIN
+0x002 constant EPOLLPRI
+0x004 constant EPOLLOUT
+
+
+struct
+  cell%   field *ptr
+  cell%   field fd
+  cell%   field u32
+  double% field u64
+end-struct epoll_data%
+
+struct
+  cell%       field events
+  epoll_data% field data
+end-struct epoll_event%
