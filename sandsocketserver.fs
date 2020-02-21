8212dd5686e2adba6abe8ec167998ef82ff669f0
harrypower <philipkingsmith@gmail.com>
2020-02-21 03:03:56 -0700

    http-response cleanup

--- sandsocketserver.fs
+++ sandsocketserver.fs
@@ -113,7 +113,7 @@ variable tempheader$
   s\" HTTP/1.1 200 OK\r\n" tempheader$ $!
   s\" Connection: close\r\n" tempheader$ $+!
   s\" Server: Sandserver 0.1\r\n" tempheader$ $+!
-\  s\" Accept-Ranges: bytes\r\n" tempheader$ $+!
+  s\" Accept-Ranges: bytes\r\n" tempheader$ $+!
   s\" Content-type: text/html; charset=utf-8\r\n" tempheader$ $+!
   tempheader$ $@ ;
 
@@ -127,7 +127,6 @@ variable tempresponse$
   s\" \r\n" tempresponse$ $+!
 
   caddr u tempresponse$ $+!
-\  s\" \r\n" tempresponse$ $+!
   s\" \r\n\r\n" tempresponse$ $+!
   tempresponse$ $@ ;
 
