c2dcceae5e5b4baf50e827fd13b7cd071ed700f0
harrypower <philipkingsmith@gmail.com>
2020-03-17 23:09:21 -0600

    more time

--- getcgi.fs
+++ getcgi.fs
@@ -29,7 +29,7 @@ warnings off
 
 s" no terminator found in stdin!" exception constant noterm
 
-200000 value cmdtimeout
+5000000 value cmdtimeout
 variable convert$
 : udto$ ( ud -- caddr u )  \ convert unsigned double to a string
     <<# #s  #> #>> convert$ $! convert$ $@ ;
