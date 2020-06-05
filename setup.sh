5417b5fb8b8678bb7242ad14d96f9b677e191386
harrypower <philipkingsmith@gmail.com>
2020-06-05 12:44:19 -0600

    adding html other commands

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo rm /var/www/html/sandtablestyle.css
 sudo rm /usr/lib/cgi-bin/directtesting.cgi
 sudo rm /var/www/html/favicon.ico
 sudo rm /var/www/html/lines.html
+sudo rm /var/www/html/othercmds.html
 
 sudo cp index.html /var/www/html/index.html
 sudo cp testing.html /var/www/html/testing.html
@@ -13,5 +14,6 @@ sudo cp sandtablestyle.css /var/www/html/sandtablestyle.css
 sudo cp directtesting.cgi /usr/lib/cgi-bin/directtesting.cgi
 sudo cp favicon.ico /var/www/html/favicon.ico
 sudo cp lines.html /var/www/html/lines.html
+sudo cp othercmds.html /var/www/html/othercmds.html
 
 sudo chmod 777 /usr/lib/cgi-bin/directtesting.cgi
