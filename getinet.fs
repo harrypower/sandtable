4936db2252efa22ed656cea69991ce6f3701a91e
Philip Smith <philipkingsmith@gmail.com>
2021-01-26 16:12:24 -0700

    comment change

--- getinet.fs
+++ getinet.fs
@@ -15,7 +15,7 @@
 \    You should have received a copy of the GNU General Public License
 \    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-\ inet sends message to this code
+\ inetd sends message to this code
 
 \ Requires:
 \ stdatafiles.fs
